9c73e6b78fc96a7a254b3912489953343def0a84
Evelina1999 <evelincik0327@gmail.com>
2022-01-28 02:43:41 +0200

    prideta keleta kintamuju

--- PatiPirmaPrograma/PatiPirmaPrograma/Program.cs
+++ PatiPirmaPrograma/PatiPirmaPrograma/Program.cs
@@ -10,9 +10,27 @@ namespace PatiPirmaPrograma
     {
         static void Main(string[] args)
         {
+            int skaicius1;
+            skaicius1 = 5;
+            int skaicius2;
+            skaicius2 = 6;
+            skaicius1 = 1;
+
+            decimal skaicius4 = 8.4m;
+            double skaicius3 = 8.1;
+            string vardas = "Petriukas";
+            Console.WriteLine(vardas);
+
+            Console.WriteLine(skaicius2 + skaicius3);
+            
+            Console.WriteLine(skaicius1 + skaicius2);
+        
+
+
             Console.WriteLine("Myliu savo mokytoją");
             Console.WriteLine("as irgi tave");
             Console.ReadLine();
+
         }
     }
 }
