d08cfb449b4b0516560bf372004646555a5140ef
Evelina1999 <evelincik0327@gmail.com>
2022-01-30 00:58:12 +0200

    init

---
+++ Staciakampis/Staciakampis/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Staciakampis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iveskite simboli");
+            int simbolis = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("{0}{0}{0} \n{0} {0}\n{0} {0}\n{0} {0}\n{0}{0}{0}", simbolis);
+            Console.ReadLine();
+        }
+
+    }
+}
