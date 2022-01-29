c865b9910f8ffba279f62e15eae6b60e7d166d35
Evelina1999 <evelincik0327@gmail.com>
2022-01-29 14:57:48 +0200

    pridejom klases

--- CS antra programa/CS antra programa/Program.cs
+++ CS antra programa/CS antra programa/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace CS_antra_programa
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
+            Rectangle r = new Rectangle();
+            Rectangle r1 = new Rectangle();
+            r.Acceptdetails(3.5d, 5.5d);
+            r.Display();
+            r1.Acceptdetails(1.5d, 4.4d);
+            r1.Display();
+            Console.ReadLine();
+        }
+
+    }
+    class Rectangle
+    {
+        //member veriables
+        private double lenght;
+        private double width;
+
+        public void Acceptdetails(double inLenght, double inWidht)
+        {
+            lenght = inLenght;
+            width = inWidht;
+        }
+        public double GetArea()
+        {
+            return lenght * width;
+        }
+        public void Display()
+        {
+            Console.WriteLine("Lenght:{0}", lenght);
+            Console.WriteLine("Width:{0}", width);
+            Console.WriteLine("Area:{0}", GetArea());
         }
     }
+
 }
