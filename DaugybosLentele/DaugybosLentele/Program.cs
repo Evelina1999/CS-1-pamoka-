694e319d3eac3867ac5a296ee12852846cbd3599
Evelina1999 <evelincik0327@gmail.com>
2022-01-29 16:31:17 +0200

    daugyba

--- DaugybosLentele/DaugybosLentele/Program.cs
+++ DaugybosLentele/DaugybosLentele/Program.cs
@@ -10,6 +10,19 @@ namespace DaugybosLentele
     {
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Iveskite bet koki skaiciu");
+            int skaicius = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(skaicius * 1);
+            Console.WriteLine(skaicius * 2);
+            Console.WriteLine(skaicius * 3);
+            Console.WriteLine(skaicius * 4);
+            Console.WriteLine(skaicius * 5);
+            Console.WriteLine(skaicius * 6);
+            Console.WriteLine(skaicius * 7);
+            Console.WriteLine(skaicius * 8);
+            Console.WriteLine(skaicius * 9);
+            Console.WriteLine(skaicius * 10);
+            Console.ReadLine();
+        } 
     }
 }
