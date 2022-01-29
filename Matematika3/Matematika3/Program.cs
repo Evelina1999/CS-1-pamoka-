2eda4b394cdef6dfcee00fb4a0e8a23aac916471
Evelina1999 <evelincik0327@gmail.com>
2022-01-30 00:23:42 +0200

    A + B * C

--- Matematika3/Matematika3/Program.cs
+++ Matematika3/Matematika3/Program.cs
@@ -10,6 +10,19 @@ namespace Matematika3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite skaiciu A");
+            int skaiciusA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu B");
+            int skaiciusB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu C");
+            int skaiciusC = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("skaiciusA + skaiciusB * skaicius c yra lygu:");
+            Console.WriteLine((skaiciusA + skaiciusB) * skaiciusC);
+            Console.WriteLine("skaiciusA * skaiciusC + skaiciusB * skaiciusC yra lygu:");
+            Console.WriteLine(skaiciusA * skaiciusC + skaiciusB * skaiciusC);
+            Console.ReadLine();
+
         }
     }
 }
