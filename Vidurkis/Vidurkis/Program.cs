74f8eddc23967724ccdfbcfb97f31222815b1110
Evelina1999 <evelincik0327@gmail.com>
2022-01-29 16:58:47 +0200

    Vidurkis

--- Vidurkis/Vidurkis/Program.cs
+++ Vidurkis/Vidurkis/Program.cs
@@ -10,6 +10,22 @@ namespace Vidurkis
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Iveskite 4 skaicius");
+            double skaicius1 = Convert.ToDouble(Console.ReadLine());
+            double skaicius2 = Convert.ToDouble(Console.ReadLine());
+            double skaicius3 = Convert.ToDouble(Console.ReadLine());
+            double skaicius4 = Convert.ToDouble(Console.ReadLine());
+
+
+            Console.WriteLine("Vidurkis: {0}", (skaicius1 + skaicius2 + skaicius3 + skaicius4) / 4 );
+            Console.ReadLine();
+
+            
+
+
+
+            
         }
     }
 }
