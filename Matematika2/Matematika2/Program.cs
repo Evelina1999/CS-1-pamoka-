ec2b5ac83aa733591651c63f76fb2b438fa277b7
Evelina1999 <evelincik0327@gmail.com>
2022-01-29 15:52:06 +0200

    Matematika2

--- Matematika2/Matematika2/Program.cs
+++ Matematika2/Matematika2/Program.cs
@@ -10,13 +10,15 @@ namespace Matematika2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Iveskite skaiciu 1");
             double skaicius1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Iveskite skaiciu 2");
             double skaicius2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.WriteLine(skaicius1 * skaicius2);
-            Console.WriteLine(skaicius1 / skaicius2);
-            Console.WriteLine(skaicius1 + skaicius2);
-            Console.WriteLine(skaicius1 - skaicius2);
+            Console.WriteLine("Skaiciu sandauga yra: {0}", skaicius1 * skaicius2);
+            Console.WriteLine("Skaiciu dalyba yra: {0}", skaicius1 / skaicius2);
+            Console.WriteLine("Skaiciu suma yra: {0}", skaicius1 + skaicius2);
+            Console.WriteLine("skaiciu skirtumas: {0}", skaicius1 - skaicius2);
             Console.ReadLine();
 
         }
