5ebdb0a2d67a62d118a5c4d370a3ed1face287b5
Evelina1999 <evelincik0327@gmail.com>
2022-01-30 14:58:46 +0200

    Temperatura

--- Temperatura/Temperatura/Program.cs
+++ Temperatura/Temperatura/Program.cs
@@ -10,6 +10,25 @@ namespace Temperatura
     {
         static void Main(string[] args)
         {
+            int celsijai;
+            int kelvinai;
+            int farenheitai;
+
+            Console.WriteLine("parasykite temperatura laipsniais C");
+
+            celsijai= Convert.ToInt32(Console.ReadLine());
+
+            kelvinai = celsijai + 273;
+            farenheitai = celsijai * 18 / 10 + 32;
+
+            Console.WriteLine("Temperatura Kelvinais: {0}", kelvinai = celsijai + 273);
+            Console.WriteLine("Temperatura Farenheitais: {0}", farenheitai = celsijai * 18 / 10 + 32);
+
+
+            Console.ReadLine();
+
+           
+            
         }
     }
 }
